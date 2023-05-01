1fb3acfbec7e0f563ff9c69bf2089a831fcc7f3e
Brent Vatne <brentvatne@gmail.com>
2023-05-01 11:04:54 -0700

    Repro attempt

--- app.config.js
+++ app.config.js
@@ -8,5 +8,16 @@ export default (config) => ({
     eas: {
       projectId: "1243b0e0-f74e-4c55-8a98-4acb3f9633dd"
     }
-  }
+  },
+  // Skip expo-apple-authentication plugin installation in production,
+  // so we will have the entitlements in our local ios directory but not
+  // when we generate entitlements for EAS Build with the production profile.
+  _internal: process.env.PRODUCTION ? {
+    "pluginHistory": {
+      "expo-apple-authentication": {
+        "name": "expo-apple-authentication",
+        "version": "UNVERSIONED"
+      }
+    }
+  } : {}
 });
